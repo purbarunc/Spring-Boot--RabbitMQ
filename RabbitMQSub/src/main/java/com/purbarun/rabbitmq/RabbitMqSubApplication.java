c5fcb9a08965cc642f404ca8e06b80bb528d6cf6
Purbarun Chakrabarti <purbarun@gmail.com>
2024-06-10 10:58:41 +0530

    Latest Code changes and improvement.

--- RabbitMQSub/src/main/java/com/purbarun/rabbitmq/RabbitMqSubApplication.java
+++ RabbitMQSub/src/main/java/com/purbarun/rabbitmq/RabbitMqSubApplication.java
@@ -6,9 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RabbitMqSubApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		SpringApplication.run(RabbitMqSubApplication.class, args);
-		while (true) {
-		}
+		Thread.currentThread().join();
 	}
 }
